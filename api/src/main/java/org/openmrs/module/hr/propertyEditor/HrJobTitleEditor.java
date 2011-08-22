7025e3e16543f0d588c0aee393d95aff3fdf4ee3
Sai Manohar Nethi <manu.raj2007@gmail.com>
2011-08-22 16:17:13 +0000

    added a property editor in process of optimizing

---
+++ api/src/main/java/org/openmrs/module/hr/propertyEditor/HrJobTitleEditor.java
@@ -0,0 +1,41 @@
+package org.openmrs.module.hr.propertyEditor;
+
+import java.beans.PropertyEditorSupport;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.hr.HRService;
+import org.openmrs.module.hr.HrJobTitle;
+import org.springframework.util.StringUtils;
+
+public class HrJobTitleEditor extends PropertyEditorSupport{
+private Log log = LogFactory.getLog(this.getClass());
+	
+	public HrJobTitleEditor(){
+		
+	}
+	public void setAsText(String text) throws IllegalArgumentException {
+		HRService hrService=Context.getService(HRService.class);
+		if (StringUtils.hasText(text)) {
+			try {
+				setValue(hrService.getJobTitleById(Integer.valueOf(text)));
+			}
+			catch (Exception ex) {
+				log.error("Error setting text" + text, ex);
+				throw new IllegalArgumentException("job title not found: " + ex.getMessage());
+			}
+		} else {
+			setValue(null);
+		}
+	}
+	
+	public String getAsText() {
+		HrJobTitle jobTitle = (HrJobTitle) getValue();
+		if (jobTitle== null) {
+			return "";
+		} else {
+			return jobTitle.getId().toString();
+		}
+	}
+}
