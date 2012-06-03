46e671b3c6525639ba5f9e912c270572d0fd9897
Sandeep Joshi <sandeep.j1989@gmail.com>
2012-06-03 04:37:54 +0000

    Post service changed to use new tets data<sandeep>

--- api/src/test/java/org/openmrs/module/hr/api/HrPostServiceTest.java
+++ api/src/test/java/org/openmrs/module/hr/api/HrPostServiceTest.java
@@ -6,6 +6,11 @@ import org.openmrs.api.context.Context;
 import org.openmrs.module.hr.HrJobTitle;
 import org.openmrs.module.hr.HrPost;
 import org.openmrs.test.BaseModuleContextSensitiveTest;
+import org.openmrs.test.SkipBaseSetup;
+import org.openmrs.test.TestUtil;
+import org.openmrs.util.RoleConstants;
+
+import javax.management.relation.Role;
 
 import static org.junit.Assert.*;
 
@@ -14,8 +19,9 @@ public class HrPostServiceTest extends BaseModuleContextSensitiveTest {
 
     @Before
 	public void before() throws Exception {
-		initializeInMemoryDatabase();
-		executeDataSet("post_service_test_data.xml");
+        executeDataSet("person_test_data.xml");
+        executeDataSet("privilege_data.xml");
+        executeDataSet("post_service_test_data.xml");
         hrPostService = Context.getService(HRPostService.class);
 	}
 
@@ -25,67 +31,98 @@ public class HrPostServiceTest extends BaseModuleContextSensitiveTest {
     }
 
     @Test
-    public void shouldGetAllISCOCodes(){
-        assertEquals(2,hrPostService.getAllIscoCodes().size());
+    @SkipBaseSetup
+    public void shouldGetAllISCOCodes() throws Exception {
+        Context.authenticate("hrmanager","Hrmanager123");
+        assertEquals(619, hrPostService.getAllIscoCodes().size());
+        Context.authenticate("admin","Admin123");
     }
 
     @Test
+    @SkipBaseSetup
     public void shouldGetISCOOcdeById(){
-        assertNotNull(hrPostService.getIscoCodeById("2"));
+        Context.authenticate("hrmanager","Hrmanager123");
+        assertNotNull(hrPostService.getIscoCodeById("1"));
+        Context.authenticate("admin","Admin123");
     }
 
     @Test
+    @SkipBaseSetup
     public void shouldGetJobTitleById(){
-        assertNotNull(hrPostService.getJobTitleById(2));
+        Context.authenticate("hrmanager","Hrmanager123");
+        assertNotNull(hrPostService.getJobTitleById(1));
+        Context.authenticate("admin","Admin123");
     }
 
     @Test
+    @SkipBaseSetup
     public void shouldRetireOrUnRetireJobTitle(){
-        HrJobTitle hrJobTitle = hrPostService.getJobTitleById(2);
+        Context.authenticate("hrmanager","Hrmanager123");
+        HrJobTitle hrJobTitle = hrPostService.getJobTitleById(1);
         hrPostService.retireJobTitle(hrJobTitle,"test reason");
-        assertTrue(hrPostService.getJobTitleById(2).isRetired());
+        assertTrue(hrPostService.getJobTitleById(1).isRetired());
         hrPostService.unretireJobTitle(hrJobTitle);
-        assertFalse(hrPostService.getJobTitleById(2).isRetired());
+        assertFalse(hrPostService.getJobTitleById(1).isRetired());
+        Context.authenticate("admin","Admin123");
+
     }
 
     @Test
+    @SkipBaseSetup
     public void shouldGetAllJobTitles(){
-        assertEquals(2,hrPostService.getAllJobTitles().size());
+        Context.authenticate("hrmanager","Hrmanager123");
+        assertEquals(31,hrPostService.getAllJobTitles().size());
+        Context.authenticate("admin","Admin123");
     }
 
     @Test
+    @SkipBaseSetup
     public void shouldSaveJobTitle(){
+        Context.authenticate("hrmanager","Hrmanager123");
         HrJobTitle hrJobTitle = hrPostService.getJobTitleById(1);
         hrJobTitle.setTitle("changed title");
         hrPostService.saveJobTitle(hrJobTitle);
         assertEquals("changed title", hrPostService.getJobTitleById(1).getTitle());
+        Context.authenticate("admin","Admin123");
     }
 
     @Test
+    @SkipBaseSetup
     public void shouldGetAllPosts(){
-        assertEquals(2, hrPostService.getAllPosts(true, true).size());
+        Context.authenticate("hrmanager","Hrmanager123");
+        assertEquals(91, hrPostService.getAllPosts(true, true).size());
+        Context.authenticate("admin","Admin123");
     }
 
     @Test
+    @SkipBaseSetup
     public void shouldGetPostById(){
-        assertNotNull(hrPostService.getPostById(1));
+        Context.authenticate("hrmanager","Hrmanager123");
+        assertNotNull(hrPostService.getPostById(7777701));
+        Context.authenticate("admin","Admin123");
     }
 
     @Test
+    @SkipBaseSetup
     public void shouldRetireOrUnRetirePost(){
-        HrPost hrPost = hrPostService.getPostById(2);
+        Context.authenticate("hrmanager","Hrmanager123");
+        HrPost hrPost = hrPostService.getPostById(7777701);
         hrPostService.retirePost(hrPost,"test reason");
-        assertTrue(hrPostService.getPostById(2).isRetired());
+        assertTrue(hrPostService.getPostById(7777701).isRetired());
         hrPostService.unretirePost(hrPost);
-        assertFalse(hrPostService.getPostById(2).isRetired());
+        assertFalse(hrPostService.getPostById(7777701).isRetired());
+        Context.authenticate("admin","Admin123");
     }
 
     @Test
+    @SkipBaseSetup
     public void shouldSavePost(){
-        HrPost hrPost = hrPostService.getPostById(2);
+        Context.authenticate("hrmanager","Hrmanager123");
+        HrPost hrPost = hrPostService.getPostById(7777701);
         hrPost.setFundingSource("MHRD");
         hrPostService.savePost(hrPost);
-        assertEquals("MHRD",hrPostService.getPostById(2).getFundingSource());
+        assertEquals("MHRD",hrPostService.getPostById(7777701).getFundingSource());
+        Context.authenticate("admin","Admin123");
     }
 
 
