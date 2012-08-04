32c5cef7bf948a75206ca94d212b9de7645c091c
Sandeep Joshi <sandeep.j1989@gmail.com>
2012-08-04 04:40:49 +0000

    modified Resource<sandeep>

--- omod/src/main/java/org/openmrs/module/hr/rest/v1_0/resource/HRTrainPersonResource.java
+++ omod/src/main/java/org/openmrs/module/hr/rest/v1_0/resource/HRTrainPersonResource.java
@@ -6,7 +6,10 @@ import org.openmrs.module.hr.HrTrainPerson;
 import org.openmrs.module.hr.HrTraining;
 import org.openmrs.module.hr.api.HRTrainingService;
 import org.openmrs.module.webservices.rest.web.RequestContext;
+import org.openmrs.module.webservices.rest.web.RestConstants;
 import org.openmrs.module.webservices.rest.web.annotation.Resource;
+import org.openmrs.module.webservices.rest.web.representation.DefaultRepresentation;
+import org.openmrs.module.webservices.rest.web.representation.FullRepresentation;
 import org.openmrs.module.webservices.rest.web.representation.Representation;
 import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingCrudResource;
 import org.openmrs.module.webservices.rest.web.resource.impl.DelegatingResourceDescription;
@@ -49,7 +52,33 @@ public class HRTrainPersonResource extends DelegatingCrudResource<HrTrainPerson>
     }
 
     @Override
-    public DelegatingResourceDescription getRepresentationDescription(Representation representation) {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    public DelegatingResourceDescription getRepresentationDescription(Representation rep) {
+        DelegatingResourceDescription Descri=new DelegatingResourceDescription();
+        if(rep instanceof DefaultRepresentation)
+        {
+            Descri.addProperty("uuid");
+            Descri.addProperty("trainingId");
+            Descri.addProperty("category");
+            Descri.addProperty("name");
+            Descri.addProperty("description");
+            Descri.addSelfLink();
+            Descri.addLink("full", ".?v="+ RestConstants.REPRESENTATION_FULL);
+            return Descri;
+        }
+        else if(rep instanceof FullRepresentation)
+        {
+            Descri.addProperty("uuid");
+            Descri.addProperty("trainingId");
+            Descri.addProperty("category");
+            Descri.addProperty("name");
+            Descri.addProperty("description");
+            Descri.addProperty("hrTrainingClasses",Representation.FULL);
+            return Descri;
+
+        }
+
+        return null;
     }
+
+
 }
